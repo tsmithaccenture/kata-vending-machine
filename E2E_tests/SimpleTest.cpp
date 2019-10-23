7ec429217271eb65262c7edd4aa6947f58ce1cc9
Travis Smith <travis.r.smith@accenture.com>
2019-10-23 11:52:28 -0500

    initialized better structure per Nate

---
+++ E2E_tests/SimpleTest.cpp
@@ -0,0 +1,6 @@
+#include "gtest/gtest.h"
+
+TEST(SimpleTestSuite, SimpleTest){
+    EXPECT_EQ(1,1);
+}
+
--- VendingMachine_lib/Tests/SimpleUnitTest.cpp
+++ VendingMachine_lib/Tests/SimpleUnitTest.cpp
@@ -1,5 +1,5 @@
 #include "gtest/gtest.h"
 
-TEST(AbsoluteDateTestSuite, ExampleDate){
+TEST(SimpleTestSuite, SimpleTest){
     EXPECT_EQ(1,1);
 }
